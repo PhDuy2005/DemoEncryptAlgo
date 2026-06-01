294f7e4f7f32c5ec04cf7564cc26b1d91449bc59
TuUyen038 <uyentu510@gmail.com>
2026-06-01 21:23:59 +0700

    feat: demo ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	testCompileOnly("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testAnnotationProcessor("org.projectlombok:lombok")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
 
 tasks.withType<Test> {
